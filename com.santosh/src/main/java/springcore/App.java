003a6dca7ec4125b858b1eaae6c83b8c67e01a7b
santosh timilsina <santos.timil@gmail.com>
2023-08-08 20:59:13 +0545

    define-bean-in-xml-file-and-get-it-through-application-context

--- com.santosh/src/main/java/springcore/App.java
+++ com.santosh/src/main/java/springcore/App.java
@@ -1,5 +1,9 @@
 package springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springcore.model.Employee;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-bean.xml");
+        Employee employee = appContext.getBean(Employee.class);
+        System.out.println(employee.getClass().getName());
     }
 }
